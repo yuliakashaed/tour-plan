1d1fa7c430a99b9c7b3decf52b54eb51e7672ad5
yuliakashaed <khatesh@mail.ru>
2021-05-12 14:10:45 +0300

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -11,5 +11,18 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
+  effect: "coverflow"
+});
+
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
 
 });
+
