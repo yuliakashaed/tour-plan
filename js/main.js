5049c844d884af10b66d7b0f470c944bb3cf6ac7
yuliakashaed <khatesh@mail.ru>
2021-05-26 09:19:09 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -119,33 +119,4 @@ menuButton.on('click', function() {
     AOS.init();
 
 
-      $(function () {
-    $("img.lazy").Lazy();
-  });
-
-
-  let map_container = document.getElementById('map_container');
-  let options_map = {
-    once: true,
-    passive: true,
-    capture: true
-  };
-  map_container.addEventListener('click', start_lazy_map, options_map);
-  map_container.addEventListener('mouseover', start_lazy_map, options_map);
-  map_container.addEventListener('touchstart', start_lazy_map, options_map);
-  map_container.addEventListener('touchmove', start_lazy_map, options_map);
-
-  let map_loaded = false;
-
-  function start_lazy_map() {
-    if (!map_loaded) {
-      let map_block = document.getElementById('ymap_lazy');
-      map_loaded = true;
-      map_block.setAttribute('src', map_block.getAttribute('data-src'));
-      map_block.removeAttribute('data-src');
-    }
-  }
-
-
-
 });
